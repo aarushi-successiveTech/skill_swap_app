b2ad5e935ecc476bc7e1b7435d4551fa70f9bc2c
Aarushi Mahajan <aarushi.mahajan@aarushivrs-24.successivetech.com>
2025-08-29 09:50:46 +0530

    swap services added

--- backend/src/service/matchService.js
+++ backend/src/service/matchService.js
@@ -1,4 +1,5 @@
 import userModel from "../model/userModel.js";
+import swapModel from "../model/swapModel.js";
 
 export const matchingService = async(userId) => {
 
@@ -8,8 +9,21 @@ export const matchingService = async(userId) => {
         return {error: 'incorrect user matching!'}
     }
 
+    const closedSwaps = await swapModel.find({
+        $or: [
+            {fromUser: userId, status: {$in: ['accepted', 'rejected']}}, 
+            {toUser: userId, status: {$in: ['accepted', 'rejected']}}
+        ]
+    }); 
+
+    const connections = closedSwaps.map(swap => {
+        return swap.fromUser.toString() === userId.toString() ? swap.toUser : swap.fromUser; 
+    }); 
+
+    connections.push(userId); 
+
     const matches = await userModel.find({
-      _id: { $ne: userId },
+      _id: { $nin: connections },
       skillOffered: { $in: currentUser.skillWanted }, 
       skillWanted: { $in: currentUser.skillOffered } 
     }).select("name email skillOffered skillWanted");
